d207a4cbc48bbe310ae8af62a5051a914ad8c45e
Panasyura <nuta_panasura@mail.ru>
2018-05-07 23:17:20 +0500

    FirstStep

---
+++ src/test/java/FrameForAdminPasswordAnyaTest.java
@@ -0,0 +1,17 @@
+
+
+
+import static org.junit.Assert.assertEquals;
+import start.FrameForAdminPassword;
+
+public class FrameForAdminPasswordAnyaTest {
+
+    @org.junit.Test
+    public void passwordTrue() {
+        assertEquals(true,new FrameForAdminPassword().password("anya"));
+    }
+    @org.junit.Test
+    public void passwordFalse() {
+        assertEquals(false,new FrameForAdminPassword().password("adsdasd"));
+    }
+}
